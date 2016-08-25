63b6711183ca9134b588e4c7a9b7f8266d8f502d
HCI01\meads <meads@hcpci.com>
2016-08-25 13:49:03 -0400

    1 of 3 : Flux

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { MongoClient } from 'mongodb';
 
 let app = express();
 
@@ -8,4 +9,21 @@ let app = express();
 
 app.use(express.static('public'));
 
-app.listen(3000);
+let db;
+let connectionString = process.env.MONGO_URL || "mongodb://meads:usfbulls23@ds031741.mlab.com:31741/rgrjs";
+
+MongoClient.connect(connectionString, (err, database) => {
+    if(err) throw err;
+
+    db = database;
+    app.listen(3000, () => {
+        console.log("listening on port 3000");
+    });
+});
+
+app.get("/data/links", (req, res) => { 
+        db.collection("links").find({}).toArray((err, links) => {
+         if(err) throw err;
+         res.json(links);
+    });
+});
