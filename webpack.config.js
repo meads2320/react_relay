9396f49b5a7b4de1c32238f6fad8379136c9f1e5
HCI01\meads <meads@hcpci.com>
2016-08-25 11:13:52 -0400

    Babel 6 update

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,14 @@
-module.exports = { 
-    entry: "./public/js/app.js",
-    output : {
-        path: __dirname + "/public",
-        filename: "bundle.js"
-    },
-    externals: {
-        React: 'react'
-    }
-}
+module.exports = {
+  entry: "./js/app.js",
+  output: {
+    path: __dirname + "/public",
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader',
+        query: { presets: ['react', 'es2015'] }
+      }
+    ]
+  }
+};
